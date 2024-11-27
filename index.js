a08494f312e9fa2c568d688ee9e150995d0bf0d9
Happiness Maina <mainahappiness361@gmail.com>
2024-11-27 15:41:51 +0300

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+    if (distance <= 400) {
+      return 'This one is on me!';
+    } else if (distance > 2500) {
+      return 'No can do.';
+    } else if (distance > 2000) {
+      return 'I will gladly take your thirty bucks.';
+    } else if (distance > 400 && distance <= 2000) {
+      return 'That will be twenty bucks.';
+    }
+  }
+  
+
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good.": city==='Pittsburgh'?"No go." :"unknown";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+      case 'not as generous':
+        return 'Thank you.'
+        default:
+          return 'Bye.'
+  }
   // Write your code here!
 }
